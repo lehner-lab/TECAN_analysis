2caed9016c0d8b716dcf15a82752d1cd5ce7d19f
Andre Faure <a.jfaure@gmail.com>
2024-04-04 18:05:40 +0200

    update with support for design file, plasmid-specific growth curves and boxplots

--- tecan.R
+++ tecan.R
@@ -129,13 +129,14 @@ calculate_growth_rates_heuristic <- function(
     tmp <- rbind(as.numeric(colnames(tecan_data)),
                  as.numeric(tecan_data[i,]))
     tmp <- tmp[,!is.na(tmp[2,])]
-    tmp <- fit_easylinear(time = tmp[1,], y = tmp[2,], h = h_parameter) ### adjust h paramater
+    tmp_fit <- fit_easylinear(time = tmp[1,], y = tmp[2,], h = h_parameter) ### adjust h paramater
 
     ### you can run plot(tmp) to check if the slope calculation looks good
     results_list[[well_ids[i]]] <- data.table(
       'well_id' = well_ids[i],
-      'maxGR' = tmp@par[['mumax']],
-      'lag' = tmp@par[['lag']])
+      'maxGR' = tmp_fit@par[['mumax']],
+      'lag' = tmp_fit@par[['lag']],
+      'maxOD' = max(tmp[2,]))
   }
   return(rbindlist(results_list))
 }
@@ -144,11 +145,43 @@ calculate_growth_rates_heuristic <- function(
 set_well_status <- function(
   input_dt,
   dead_threshold,
-  lag_threshold
+  lag_threshold,
+  design_path
   ){
   input_dt[, status := 'PASS']
-  input_dt[maxGR<dead_threshold, status := paste0('FAIL (maxGR<', dead_threshold, ')')]
+  input_dt[maxGR<dead_threshold | maxOD<od_threshold, status := paste0('FAIL (maxGR<', dead_threshold, ')')]
   input_dt[lag>lag_threshold, status := paste0('FAIL (lag>', lag_threshold, ')')]
+
+  #Check if design path exists
+  if(is.null(design_path)){
+    return(input_dt)
+  }else if(!file.exists(design_path)){
+    return(input_dt)
+  }
+
+  #Load design
+  design_dt <- fread(design_path)
+  #Check if Well column exists
+  if(!"Well" %in% names(design_dt)){
+    stop("Well column missing in plate design file ('--designPath').", call. = FALSE)
+  }
+  #Check all well_ids exist in Well column
+  if(sum(!input_dt[,well_id] %in% design_dt[,Well])!=0){
+    stop("One of more wells missing in plate design file ('--designPath').", call. = FALSE)
+  }
+  #Check if Plasmid column exists
+  if(!"Plasmid" %in% names(design_dt)){
+    print("Warning: Plasmid column missing in plate design file ('--designPath'). Plasmid-specific growth curves and boxplots will not be produced.", call. = FALSE)
+  }else if(!"Well class" %in% names(design_dt)){
+    print("Warning: 'Well class' column missing in plate design file ('--designPath'). Plasmid-specific boxplots will not be produced.", call. = FALSE)
+  }
+  design_dt <- design_dt[,.SD,,.SDcols = names(design_dt)[names(design_dt) %in% c("Well", "Plasmid", "Well class")]]
+
+  #Merge results with design
+  names(design_dt)[names(design_dt)=='Well'] <- 'well_id'
+  names(design_dt) <- gsub(" ", "_", names(design_dt))
+  input_dt <- merge(input_dt, design_dt[], by = 'well_id', all.x = T)
+
   return(input_dt)
 }
 
@@ -186,7 +219,7 @@ plot_growth_curves <- function(
   names(plot_cols) <- plot_dt[, status_id]
   plot_dt[, status := factor(status, levels = unique(status))]
   d <- ggplot2::ggplot(plot_dt,ggplot2::aes(time, OD, color = status_id)) +
-    ggplot2::geom_line(linewidth = 0.5) +
+    ggplot2::geom_line(size = 0.5) +
     ggplot2::xlab("Time (h)") +
     ggplot2::ylab("Optical density") +
     ggplot2::facet_wrap(~status, ncol = 1) +
@@ -196,7 +229,68 @@ plot_growth_curves <- function(
   }else{
     d <- d + ggplot2::scale_colour_manual(values = plot_cols, breaks = names(plot_cols)[!grepl("PASS", names(plot_cols))])
   }
-  ggplot2::ggsave(paste0(outputPrefix, '.pdf'), d, width = 8, height = 8, useDingbats=FALSE)
+  ggplot2::ggsave(paste0(outputPrefix, '.pdf'), d, width = plot_width, height = plot_height, useDingbats=FALSE)
+
+  #Individual traces per plasmid
+  plasmid_dir <- paste0(outputPrefix, "_plasmids")
+  if('Plasmid' %in% names(input_dt)){
+    dir.create(plasmid_dir, showWarnings = FALSE)
+    for(i in unique(input_dt[,Plasmid])){
+      plot_dt_plasmid <- plot_dt[well_id %in% input_dt[Plasmid==i,unlist(well_id)]]
+      d <- ggplot2::ggplot(plot_dt_plasmid,ggplot2::aes(time, OD, color = status_id, linetype = well_id)) +
+        ggplot2::geom_line(size = 0.5) +
+        ggplot2::xlab("Time (h)") +
+        ggplot2::ylab("Optical density") +
+        ggplot2::coord_cartesian(ylim = range(plot_dt[,OD])) +
+        ggplot2::theme_bw()
+      if(plot_dt_plasmid[status!="PASS",.N]>20){
+        d <- d + ggplot2::scale_colour_manual(values = plot_cols, breaks = names(plot_cols)[!grepl("PASS", names(plot_cols))], guide="none")
+      }else{
+        d <- d + ggplot2::scale_colour_manual(values = plot_cols, breaks = names(plot_cols)[!grepl("PASS", names(plot_cols))])
+      }
+      ggplot2::ggsave(file.path(plasmid_dir, paste0(basename(outputPrefix), "_", i, '.pdf')), d, width = plot_width, height = plot_height, useDingbats=FALSE)
+
+    }
+  }
+}
+
+#Plot growth boxplots
+plot_growth_boxplots <- function(
+  tecan_data,
+  input_dt,
+  pass_proportion_threshold = 0.5){
+
+  #Check if required columns present
+  if(!'Plasmid' %in% names(result_dt) | !'Well_class' %in% names(result_dt)){
+    return()
+  }
+
+  #Format plot data
+  plot_dt <- copy(input_dt)
+  plot_dt[, plot_class := 'sample']
+  plot_dt[Well_class!="Sample", plot_class := 'remainder']
+  #Sort plasmids by median maxGR and set plasmid pass status
+  sort_dt <- plot_dt[,.(median_maxGR = median(maxGR), plasmid_pass = (sum(status=='PASS'))/.N>pass_proportion_threshold),.(Plasmid, plot_class)]
+  plot_dt[, Plasmid_factor := factor(Plasmid, levels = sort_dt[order(plot_class, median_maxGR),Plasmid])]
+  plot_dt <- merge(plot_dt, sort_dt[,.(Plasmid, plasmid_pass)], by = "Plasmid")
+  #Box status
+  plot_dt[, box_status := Well_class]
+  plot_dt[Well_class=="Sample", box_status := 'FAIL']
+  plot_dt[Well_class=="Sample" & plasmid_pass==TRUE, box_status := 'PASS']
+  plot_dt[!Well_class %in% c("Positive control", "Negative control", "Sample"), box_status := 'Remainder']
+  #Box status colours
+  gg_cols = gg_color_hue(3)
+  plot_cols <- c("dark grey", gg_cols[1], gg_cols[2], "light grey", gg_cols[3])
+  names(plot_cols) <- c("Remainder", "Negative control", "Positive control", "FAIL", "PASS")
+  d <- ggplot2::ggplot(plot_dt,ggplot2::aes(Plasmid_factor, maxGR, fill = box_status)) +
+    ggplot2::geom_boxplot(outlier.shape = NA) +
+    ggplot2::geom_jitter(color = 'black', ggplot2::aes(shape = status)) +
+    ggplot2::xlab("Sample") +
+    ggplot2::ylab("Maximum growth rate") +
+    ggplot2::theme_bw() +
+    ggplot2::scale_fill_manual(values = plot_cols) +
+    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, hjust = 1, vjust = 0.5))
+  ggplot2::ggsave(paste0(outputPrefix, '_boxplots.pdf'), d, width = plot_width, height = plot_height, useDingbats=FALSE)
 }
 
 ###########################
@@ -212,12 +306,16 @@ parser <- arg_parser(description = "Your tool description")
 parser <- add_argument(parser, "excel_path", help = "Path to the Excel file")
 
 #Add Optional Argument for Integer
-parser <- add_argument(parser, "--method", default='heuristic', help = "Maximum growth rate method")
-parser <- add_argument(parser, "--parameter", type = "integer", default=15, help = "h-parameter; number of consecutive time points to evaluate maximum growth rate")
-parser <- add_argument(parser, "--wells", default='all', help = "Comma-separated list of well ids")
-parser <- add_argument(parser, "--deadThreshold", type = "double", default=0.05, help = "Growth rate threshold for dead variants")
-parser <- add_argument(parser, "--lagThreshold", type = "double", default=48.0, help = "Lag time threshold for problematic variants")
+parser <- add_argument(parser, "--method", default = 'heuristic', help = "Maximum growth rate method")
+parser <- add_argument(parser, "--parameter", type = "integer", default = 15, help = "h-parameter; number of consecutive time points to evaluate maximum growth rate")
+parser <- add_argument(parser, "--wells", default = 'all', help = "Comma-separated list of well ids")
+parser <- add_argument(parser, "--deadThreshold", type = "double", default = 0.05, help = "Growth rate threshold for dead variants")
+parser <- add_argument(parser, "--lagThreshold", type = "double", default = 48.0, help = "Lag time threshold for problematic variants")
 parser <- add_argument(parser, "--outputPrefix", help = "Output path prefix (default: no output file; print results to stdout)")
+parser <- add_argument(parser, "--designPath", default = NULL, help = "Path to the plain text file with Well, Plasmid and Well class columns (optional)")
+parser <- add_argument(parser, "--plotWidth", type = "integer", default = 8, help = "Plot width in inches (default:8)")
+parser <- add_argument(parser, "--plotHeight", type = "integer", default = 8, help = "Plot height in inches (default:8)")
+parser <- add_argument(parser, "--ODThreshold", type = "double", default = 0, help = "Minimum optical density required to escape 'deadThreshold' (default:0)")
 
 #Parse the Command Line Arguments
 args <- parse_args(parser)
@@ -257,6 +355,10 @@ wells <- unlist(strsplit(args[['wells']], ','))
 dead_threshold <- args[['deadThreshold']]
 lag_threshold <- args[['lagThreshold']]
 outputPrefix <- args[['outputPrefix']]
+design_path <- args[['designPath']]
+plot_width <- args[['plotWidth']]
+plot_height <- args[['plotHeight']]
+od_threshold <- args[['ODThreshold']]
 
 ###########################
 ### IMPORT PLATE DATA MATRIX
@@ -283,13 +385,22 @@ if(method == "heuristic"){
 result_dt <- set_well_status(
     input_dt = result_dt,
     dead_threshold = dead_threshold,
-    lag_threshold = lag_threshold)
+    lag_threshold = lag_threshold,
+    design_path = design_path)
 
 ###########################
 ### PLOT GROWTH CURVES
 ###########################
 
 plot_growth_curves(
+  tecan_data = tecan_mat,
+  input_dt = result_dt)
+
+###########################
+### PLOT GROWTH BOXPLOTS
+###########################
+
+plot_growth_boxplots(
   tecan_data = tecan_mat,
   input_dt = result_dt)
 
